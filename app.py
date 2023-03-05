a35e8aef82aded015cb3efd1f27287a8ccf991b0
poolkit <pulkit.pk7@gmail.com>
2023-03-05 19:57:21 +0530

    adding fastapi

---
+++ app.py
@@ -0,0 +1,16 @@
+import uvicorn
+from fastapi import FastAPI, File, UploadFile
+
+app = FastAPI()
+
+@app.get("/")
+def home_page():
+    return {"Hello": "Pupu"}
+
+
+@app.post("/items")
+async def read_item(id: int, image: UploadFile = File(...)):
+    return {"item_id": id, "item_image_name": image.filename}
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=5000) 
